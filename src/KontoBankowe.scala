b21f79c021518a67304ac62ecfc47f07922cfb90
dajana0 <dajana.kubica@gmail.com>
2018-04-09 20:40:21 +0200

    sync

--- src/KontoBankowe.scala
+++ src/KontoBankowe.scala
@@ -2,7 +2,7 @@
     private var _stanKonta = stanKonta;
    def balance = _stanKonta
   def this() {
-    this(_stanKonta = 0.0);
+    this( 0.0);
   }
     def wplata(ile:Double){
       _stanKonta = _stanKonta+ile;
@@ -20,7 +20,7 @@
 
 object Demo{
   def main(args: Array[String]): Unit = {
-    val kb = new KontoBankowe(40)
+    val kb = new KontoBankowe();
     kb.wplata(10);
     println(kb.balance);
     kb.wyplata(20);
